258c3ece9ca0a889676a4ee1eb0f45fc4b7360f3
Igor Varga <ivarga@cybervisiontech.com>
2015-04-28 19:53:55 +0300

    [UI] reowrk frontend

--- first_variant/first_variant/settings.py
+++ first_variant/first_variant/settings.py
@@ -15,7 +15,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
+PROJECT_DIR = os.path.dirname(__file__)
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.8/howto/deployment/checklist/
 
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main_app'
 )
 
 MIDDLEWARE_CLASSES = (
@@ -70,7 +71,7 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'first_variant.wsgi.application'
 
-
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
--- first_variant/main_app/views.py
+++ first_variant/main_app/views.py
@@ -1,3 +1,4 @@
+from django.conf.global_settings import INSTALLED_APPS
 from django.shortcuts import render, render_to_response
 from django.http.response import HttpResponse
 from django.template import Template, loader, Context
@@ -7,6 +8,7 @@ from mako.lookup import TemplateLookup
 
 
 def basic(request):
+    print(str(INSTALLED_APPS))
     # mylookup = TemplateLookup(directories=['/docs'], output_encoding='utf-8', encoding_errors='replace')
     # mytemplate = mylookup.get_template("/home/igorv/server/DJ/first_variant/main_app/templates/main.mako")
     t = loader.get_template("main.mako")
@@ -16,5 +18,5 @@ def basic(request):
     # print(templ.render())
     # print(mytemplate.render())
 
-    return render_to_response("main.mako")
+    return render_to_response("main.mako", {"foo": ['vollid', 'fsdfsd', 'fsdfsdf', 'fsdfsdf']})
 pass
