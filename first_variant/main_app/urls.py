4529c562996cf2ad29730ebf908673a405842664
IVarha <ivarga@maprtech.com>
2015-06-16 21:37:09 +0300

    regression

--- first_variant/main_app/urls.py
+++ first_variant/main_app/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls  import patterns, include, url
 #import django.conf.url
 urlpatterns = patterns('',
 	url(r'^1', 'main_app.views.basic'),
-  url(r'^post','main_app.views.test')
+  url(r'^post','main_app.views.testgraphic')
 
 
 	)
--- first_variant/main_app/views.py
+++ first_variant/main_app/views.py
@@ -2,15 +2,15 @@ from django.conf.global_settings import INSTALLED_APPS
 from django.shortcuts import render, render_to_response
 from django.http.response import HttpResponse
 from django.template import Template, loader, Context
+from nvd3 import lineChart
 from mako.lookup import TemplateLookup
-
+from sklearn import linear_model
+from scipy.integrate import trapz
 # Create your views here.
 
 
 def basic(request):
     print(str(INSTALLED_APPS))
-    # mylookup = TemplateLookup(directories=['/docs'], output_encoding='utf-8', encoding_errors='replace')
-    # mytemplate = mylookup.get_template("/home/igorv/server/DJ/first_variant/main_app/templates/main.mako")
     t = loader.get_template("main.mako")
 
     xdata = range(100)
@@ -29,12 +29,63 @@ def basic(request):
             'jquery_on_ready': False,
         }
     }
-
-    # view = 'basic'
-    # html = "<html><body>this is %s view</body></html>" % view
-
-    # print(templ.render())
     # print(mytemplate.render())
     return render_to_response("index.html", data)
     # return render_to_response("main.mako", {"foo": ['vollid', 'fsdfsd', 'fsdfsdf', 'fsdfsdf']})
 pass
+
+
+def testgraphic(request):
+    if request.POST:
+        type = 'lineChart'
+        chart = lineChart(name=type,  x_is_date=False)
+
+        #
+        xdata = range(100)
+        ydata = [x ** 3 + 234 for x in range(100)]
+        chart.add_serie(y=ydata,x=xdata,name='sas')
+        # chartdata = {'x': xdata, 'y': ydata}
+        # charttype = "lineChart"
+        # chartcontainer = 'piechart_container'
+        # data = {
+        #     'charttype': charttype,
+        #     'chartdata': chartdata,
+        #     'chartcontainer': chartcontainer,
+        #     'extra': {
+        #         'x_is_date': False,
+        #         'x_axis_format': '',
+        #         'tag_script_js': True,
+        #         'jquery_on_ready': False,
+        #     }
+        # }
+        return HttpResponse(chart.htmlcontent)
+
+
+
+def regression_main(data,xstart,xend):
+  temp = [regression(x) for x in data]
+  tmp2 = []
+  for i in range(0,len(temp)):
+    x = integrate(temp[i].prefict,xstart,xend,0.1)
+    tmp2.append((x,i))
+  mt = tmp2[0]
+  for x in tmp2:
+    if mt[0] < x[0] :
+      mt = x
+  return temp[mt[1]]
+  pass
+
+def regression(data):
+  regr = linear_model.LinearRegression()
+  regr.fit(data[0],data[1])
+  return regr
+  pass
+
+def integrate(func,start,end,dx):
+  n = int((end - start)/dx)
+  tx = []
+  for i in range(0,n):
+    tx.append(start + i*dx)
+  return trapz(func,x=tx)
+
+
